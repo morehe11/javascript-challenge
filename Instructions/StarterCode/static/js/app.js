fa8459c5a2a2a7ef94b2d450651baa6898604a2a
Jenna_Morehead <morehe11@msu.edu>
2021-06-19 18:14:55 -0400

    first attempt at table

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,33 @@
 // from data.js
 var tableData = data;
-
+var tbody = d3.select("tbody")
 // YOUR CODE HERE!
+data.forEach(function(tableData){
+    console.log(tableData)
+});
+
+
+data.forEach(function(tableData){
+    console.log(tableData);
+    var row = tbody.append("tr");
+});
+
+data.forEach(function(tableData){
+    console.log(tableData);
+    var row = tbody.append("tr");
+
+    Object.entries(tableData).forEach(function([key,value]){
+        console.log(key,value)
+    })
+});
+
+data.forEach(function(tableData){
+    console.log(tableData);
+    var row = tbody.append("tr");
+
+    Object.entries(tableData).forEach(function([key,value]){
+        console.log(key,value);
+        var cell = row.append("th");
+        cell.text(value);
+    })
+});
